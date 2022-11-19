e6900e90665cebc7de05c1b5ce20791eeac33646
AlinaRinn <nik17042001@rambler.ru>
2022-11-20 00:47:57 +0300

    tmp step

--- Dispersion/Dispersion.cpp
+++ Dispersion/Dispersion.cpp
@@ -7,22 +7,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 
-int main()
-{
+const int STRINGS = 3000000;
+const int COLUMNS = 10;
+
+int main() {
     std::string line;
-    std::vector<std::string> lines;
+
+    unsigned int** input_array = new unsigned int* [STRINGS]; // strings
+    for (int count = 0; count < STRINGS; count++) {
+        input_array[count] = new unsigned int[COLUMNS]; // cols
+    }
+    unsigned long int* dispersion_array = new unsigned long int [STRINGS]; // strings
+    unsigned int* math_array = new unsigned int[STRINGS]; // strings
+
     std::ifstream in("minmax.txt");
 
     if (in.is_open()) {
+        int i = 0;
         while (getline(in, line)) {
-            lines.push_back(line);
+           sscanf(line.c_str(), "%d %d %d %d %d %d %d %d %d %d", &input_array[i][0], &input_array[i][1], &input_array[i][2], &input_array[i][3], &input_array[i][4], &input_array[i][5], &input_array[i][6], &input_array[i][7], &input_array[i][8], &input_array[i][9]);
+           i++;
         }
     }
     else {
         std::cout << "File not found" << std::endl;
     }
-
     in.close();
+
+    for (int i = 0; i < STRINGS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+
+        }
+    }
+
+    for (int count = 0; count < COLUMNS; count++) {
+        delete[] input_array[count];
+    }
+    delete[] dispersion_array;
+    delete[] math_array;
+    std::cout << "Finish" << std::endl;
 }
